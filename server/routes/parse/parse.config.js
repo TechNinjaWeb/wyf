afbbdaa24c17e49a3491f323ed36e4b6621dfe5a
Rahim <phreelyfe@gmail.com>
2017-05-29 08:27:01 -0400

    auto-saving

--- server/routes/parse/parse.config.js
+++ server/routes/parse/parse.config.js
@@ -1,11 +1,16 @@
 let server = require('./parse-server.config.json');
 let dashboard = require('./parse-dashboard.config.json');
 
-let serverURL = `http://localhost:${process.env.PORT}/parse`;
+let localhostURL = `http://localhost:${process.env.PORT}/parse`;
+let productionURL = process.env.HOST || "https://wyf-app.herokuapp.com";
+
+let serverURL = null;
+if (process.env.HOST !== 'localhost') serverURL = productionURL;
+else serverURL = localhostURL;
 
 // Set server url to dynamic port
-dashboard.apps[0].serverURL = serverURL;
 server.serverURL = serverURL;
+for (let app of dashboard.apps) { app.serverURL = serverURL; }
 
 exports.server = server;
 exports.dashboard = dashboard;
